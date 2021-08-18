c86b56e1298ddcdb96247ec76f37003079e3af90
Ilaria Callegari <ilaria.callegari@gmail.com>
2021-08-18 17:00:35 +0100

    change file name

--- script.js
+++
@@ -1,6 +0,0 @@
-const slider = document.querySelector("#slider");
-
-slider.oninput = function () {
-  let value = ((this.value - this.min) / (this.max - this.min)) * 100;
-  this.style.backgroundImage = `linear-gradient(to right,  #f48434 0%, #f48434 ${value}%, #ffff ${value}%, #fff 100%)`;
-};
