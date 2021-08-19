ace4031035fcd8b82ad9bdbada52912d1ac5118c
Ilaria Callegari <ilaria.callegari@gmail.com>
2021-08-19 20:01:30 +0100

    change track color

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const slider = document.querySelector("#slider");
 
 slider.oninput = function () {
   let value = ((this.value - this.min) / (this.max - this.min)) * 100;
-  this.style.backgroundImage = `linear-gradient(to right,  #f48434 0%, #f48434 ${value}%, #ffff ${value}%, #fff 100%)`;
+  this.style.background = `linear-gradient(to right,  #f48434 0%, #f48434 ${value}%, #ecf0f3 ${value}%, #ecf0f3 100%)`;
 };
