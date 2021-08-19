03374c3c069d06caf8b7f1a6cbd094c6c0c0f51f
Ilaria Callegari <ilaria.callegari@gmail.com>
2021-08-19 21:13:13 +0100

    change tracker color and padding

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const slider = document.querySelector("#slider");
 
 slider.oninput = function () {
   let value = ((this.value - this.min) / (this.max - this.min)) * 100;
-  this.style.background = `linear-gradient(to right,  #f48434 0%, #f48434 ${value}%, #ecf0f3 ${value}%, #ecf0f3 100%)`;
+  this.style.background = `linear-gradient(to right,  #fE9A15 0%, #fE9A15 ${value}%, #ecf0f3 ${value}%, #ecf0f3 100%)`;
 };
